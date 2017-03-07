85733f24d518fea18a96ddf8bd003da2eb89fbc1
kamilpodlasek <admin@kamilpodlasek.pl>
2017-03-07 17:50:59 +0100

    Updated About

--- src/modules/About.js
+++ src/modules/About.js
@@ -5,17 +5,13 @@ export default React.createClass({
     return (
     	<div>
    			<h2 className="text-center">O mnie</h2>
-			<h3>Nazywam się Kamil Podlasek, urodziłem się pechowego dnia - 29 lutego 1996 roku. Tak, mam urodziny co cztery lata.</h3>
-			<h3>Ukończyłem klasę o profilu matematycznym w I Liceum im. Bolesława Chrobrego w Pszczynie, a aktualnie studiuję Informatykę na Wydziale Matematyki Stosowanej Politechniki Śląskiej.</h3>
-			<h3>Tworzę strony i aplikacje internetowe, zajmuję się głównie front-endem.</h3>
-			<h3>W projektach używam takich języków i technologii jak HTML5, CSS3, SCSS, Bootstrap, JavaScript (ES6), jQuery, React, Gulp, Webpack, Git, Node.js, PHP, MySQL oraz stosuję zasady RWD.
-			Staram się, aby każdy tworzony przeze mnie design był "pixel-perfect".
-			Ponadto bez przerwy poznaję coraz lepsze sposoby na tworzenie coraz lepszych rzeczy.</h3>
-			<h3>Jestem minimalistą i perfekcjonistą, co staram się przekładać na tworzony przeze mnie kod.</h3>
-			<h3>Pasjonuję się piłką nożną i muzyką.
-			Poza kibicowaniem i słuchaniem samemu kopię i grywam na instrumentach, głównie na gitarze basowej.</h3>
-			<h3>Ponadto lubię czasem coś ugotować - moja pizza i lazania znalazły sobie już kilku zwolenników (chyba, że nie chcieli żeby mi było przykro).</h3>
+				<h3>Nazywam się Kamil Podlasek, urodziłem się 29 lutego 1996 roku. Tak, mam urodziny co cztery lata.</h3>
+				<h3>Studiuję Informatykę na Wydziale Matematyki Stosowanej Politechniki Śląskiej.</h3>
+				<h3>Zajmuję się tworzeniem stron internetowych, głównie od strony front-endu.</h3>
+				<h3>W projektach używam między innymi takich języków i technologii, jak HTML5, CSS3, Bootstrap, JavaScript (ES6/TS), Angular, React oraz jQuery.</h3>
+				<h3>Jestem minimalistą i perfekcjonistą, co staram się przekładać na tworzony przeze mnie kod.</h3>
+				<h3>Pasjonuję się piłką nożną i muzyką. Poza kibicowaniem i słuchaniem samemu kopię i grywam na instrumentach, głównie na gitarze basowej.</h3>
     	</div>
-	);
+		);
   }
 })
